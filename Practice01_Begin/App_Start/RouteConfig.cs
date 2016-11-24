5317776aaed00c6e985e91ad9437052a28826ebe
nagahashiHome <nagahashi0@yahoo.co.jp>
2016-11-24 22:16:05 +0900

    route

--- Practice01_Begin/App_Start/RouteConfig.cs
+++ Practice01_Begin/App_Start/RouteConfig.cs
@@ -13,11 +13,19 @@ namespace Practice01_Begin
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //デフォルトルート
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            //Greetingルート
+            routes.MapRoute(
+                name: "Greeting",
+                url: "Greeting/Show",
+                defaults: new { controller = "Hello", action = "ShowHelloMessage" }
+            );
+
         }
     }
 }
