7d02d3f15344900d627f529358dabe11ba00d667
nagahashi <nagahashi>
2016-11-24 11:54:00 +0900

    新規作成

---
+++ Practice01_Begin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Practice01_Begin.Startup))]
+namespace Practice01_Begin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
